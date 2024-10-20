03adb7d72aa89e8274b2e86b391b6a7a00a9fe52
Guilherme220501 <guilherme8876sk@gmail.com>
2024-10-19 23:38:24 -0300

    njnj

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $Nome = htmlspecialchars($_POST['Nome']);
+    $Idade = htmlspecialchars($_POST['Idade']);
+    $Email = htmlspecialchars($_POST['Email']);
+    $Telefone = htmlspecialchars($_POST['Telefone']);
+    $RG = htmlspecialchars($_POST['RG']);
+    $CPF = htmlspecialchars($_POST['CPF']);
+    $Endereço = htmlspecialchars($_POST['Endereço']);
+
+    echo "<h1>Informações do usuário:</h1>";
+    echo "Nome: " . $Nome . "<br>";
+    echo "Idade: " . $Idade . "<br>";
+    echo "Email: " . $Email . "<br>";
+    echo "Telefone: " . $Telefone . "<br>";
+    echo "Endereço: " . $Endereço . "<br>";
+    echo "CPF: " . $CPF . "<br>";
+    echo "RG: " . $RG . "<br>";
+} else {
+    echo "Nenhum dado foi enviado.";
+}
+?>
